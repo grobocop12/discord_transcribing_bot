6b6bc3b7e974c0ef0a9e3556fe3ab946e51b0d9a
Grobocop <chasnik97876@gmail.com>
2021-09-12 13:35:29 +0200

    added leaving logic

--- src/leave.ts
+++ src/leave.ts
@@ -1,5 +1,26 @@
-import { Client, CommandInteraction, Snowflake } from "discord.js";
+import { getVoiceConnection } from "@discordjs/voice";
+import { Client, CommandInteraction, GuildMember, Snowflake } from "discord.js";
 
 export async function leave(interaction: CommandInteraction, client: Client, recordable?: Set<Snowflake>) {
-    
+    if (interaction.guildId &&
+        interaction.member instanceof GuildMember &&
+        interaction.member.voice.channel) {
+        destroyConnectionAndClearRecordables(interaction, interaction.member.voice.channel.id, recordable);
+    } else {
+        interaction.reply('Join a voice channel and then try again!');
+    }
+}
+
+function destroyConnectionAndClearRecordables(interaction: CommandInteraction,
+    channelId: string,
+    recordable?: Set<Snowflake>) {
+    const connection = getVoiceConnection(interaction.guildId!);
+    if (connection &&
+        connection.joinConfig.channelId === channelId) {
+        recordable?.clear();
+        connection.destroy();
+        interaction.reply('Leaving channel');
+    } else {
+        interaction.reply('You have to be in the same voice channel')
+    }
 }
